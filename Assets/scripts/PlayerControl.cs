8a441376728e9f1861d619dff593179a6b5d4a61
Rajeev Cyrus <rcyrus@gmail.com>
2014-06-01 16:03:44 -0500

    And we're done! 623623623

--- Assets/scripts/PlayerControl.cs
+++ Assets/scripts/PlayerControl.cs
@@ -4,14 +4,42 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour
 {
 	public float speed;
+	public GUIText countText;
+	public GUIText winText;
+	private int count;
+	private int boxCount;
+	void Start ()
+	{
+		boxCount = GameObject.FindGameObjectsWithTag("PickUp").Length;
+		winText.text = "";
+		count = 0;
+		SetCountText ();
+	}
 
 	void FixedUpdate ()
 	{
-		float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis("Vertical");
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
 
-		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
-		rigidbody.AddForce(movement * speed * Time.deltaTime);
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= boxCount) {
+			winText.text = "Winner is you!";
+		}
 	}
 }
